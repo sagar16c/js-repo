6f44ff7a252fdb426c663e3eb6271ffd4f51e438
sagar16c <112206074+sagar16c@users.noreply.github.com>
2024-07-31 18:28:45 +0000

    updated conversionOperations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,33 +1,71 @@
-let score = true
+// let score = true
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score);
-console.log(typeof(valueInNumber));
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
 
-console.log(valueInNumber);// nan
+// console.log(valueInNumber);// nan
 
-//if converted to Number
-// nan -> number
-// null -> 0
-// undefined -> nan
-// boolean -> 1,0
-// string -> nan
+// //if converted to Number
+// // nan -> number
+// // null -> 0
+// // undefined -> nan
+// // boolean -> 1,0
+// // string -> nan
 
-let isLoggedIn = "sagar"
+// let isLoggedIn = "sagar"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-// 1-> true
-// 0 ->false
-// ""-> false
-// "sagar"-> true
+// // 1-> true
+// // 0 ->false
+// // ""-> false
+// // "sagar"-> true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
+// let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+//**************Operations******************//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "sagar"
+let str2 = "singh"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// let num1,num2,num3
+
+// num1= num2 = num3 = 2+2
+
+// let gameCounter = 100
+// gameCounter++
+// console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
